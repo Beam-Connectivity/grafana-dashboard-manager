ec056c76191de4fd22dbdb3857b0b920c02aff88
Maxim Styushin <makcimkos@gmail.com>
2022-10-27 12:39:20 +0400

    Allow token-based authentication (#7)
    Thanks @mstyushin, @meetmatt, @LawrenceWarren 
    * add token-based auth method
    * use six library for type checking

--- grafana_dashboard_manager/api.py
+++ grafana_dashboard_manager/api.py
@@ -11,15 +11,30 @@ Wrapper interface for the REST API to grafana
 
 import json
 import logging
-import sys
 from dataclasses import dataclass
 from typing import Dict
 
 import requests
+import six
 
 logger = logging.getLogger()
 logging.getLogger("requests").setLevel(logging.WARNING)
 logging.getLogger("urllib3").setLevel(logging.WARNING)
+
+
+class TokenAuth(requests.auth.AuthBase):
+    """
+    Authentication using a Grafana API token.
+    """
+
+    def __init__(self, token: str):
+        self.token = token
+
+    def __call__(self, request: requests.models.Request):
+        request.headers.update({
+            "Authorization": f"Bearer {self.token}"
+        })
+        return request
 
 
 class RestApiBasicAuth:
@@ -27,34 +42,45 @@ class RestApiBasicAuth:
     HTTP REST calls with status code checking and common auth/headers
     """
 
-    def __init__(self, host: str = "", username: str = "", password: str = "") -> None:
+    def __init__(self, host: str = "", credentials = None) -> None:
         self.host = host
-        self.username = username
-        self.password = password
+        self.session = requests.Session()
+        self.session.headers = {
+            "Accept": "application/json; charset=UTF-8"
+        }
+        if credentials is None:
+            pass
+        elif isinstance(credentials, six.string_types):
+            self.session.auth = TokenAuth(credentials)
+        else:
+            self.session.auth = requests.auth.HTTPBasicAuth(*credentials)
 
     def get(self, resource: str) -> Dict:
         """HTTP GET"""
-        response = requests.get(f"{self.host}/api/{resource}", auth=(self.username, self.password))
+        response = self.session.request("GET", f"{self.host}/api/{resource}")
         return self._check_response(response.status_code, json.loads(response.text))
 
     def post(self, resource: str, body: dict) -> Dict:
         """HTTP POST"""
-        response = requests.post(
+        response = self.session.request(
+            "POST",
             f"{self.host}/api/{resource}",
-            auth=(self.username, self.password),
-            json=body,
-            headers={"Content-type": "application/json"},
+            json=body
         )
         return self._check_response(response.status_code, json.loads(response.text))
 
     def put(self, resource: str, body: dict) -> Dict:
         """HTTP PUT"""
-        response = requests.put(f"{self.host}/api/{resource}", auth=(self.username, self.password), data=body)
+        response = self.session.request(
+            "PUT",
+            f"{self.host}/api/{resource}",
+            data=body
+        )
         return self._check_response(response.status_code, json.loads(response.text))
 
     def delete(self, resource: str) -> Dict:
         """HTTP DELETE"""
-        response = requests.delete(f"{self.host}/api/{resource}", auth=(self.username, self.password))
+        response = self.session.request("DELETE", f"{self.host}/api/{resource}")
         return self._check_response(response.status_code, json.loads(response.text))
 
     @staticmethod
@@ -71,8 +97,7 @@ class GrafanaAPI:
     """Container for API config and the api access interface"""
 
     host: str = ""
-    username: str = ""
-    password: str = ""
+    credentials = None
     api: RestApiBasicAuth = RestApiBasicAuth()
 
 
