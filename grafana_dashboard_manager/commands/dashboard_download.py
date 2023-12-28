256c141c21901b138d24ead0bb91c1585a2676dd
Vince Chan <15341873+chan-vince@users.noreply.github.com>
2023-12-28 22:58:11 +0000

    #21 dashboard character escaping

--- grafana_dashboard_manager/commands/dashboard_download.py
+++ grafana_dashboard_manager/commands/dashboard_download.py
@@ -56,9 +56,8 @@ def download_dashboards(config: GlobalConfig, client: GrafanaApi):
 
     for folder_title, folder in folder_dashboards.items():
         for dashboard in folder.dashboards:
-            # Replace any slashes with dashes. When uploading, this cannot be reversed
-            folder_title = folder_title.replace("/", "-").replace("\\", "-")
-            dest_file_path = destination_dir / folder_title / f"{dashboard.title}.json"
+            escaped_dashboard_title = dashboard.title.replace("/", "-").replace("\\", "-").replace(" ", "_")
+            dest_file_path = destination_dir / folder_title / f"{escaped_dashboard_title}.json"
             client.dashboards.save(dashboard.uid, dest_file_path)
         logger.info(f"Saved {len(folder.dashboards)} dashboards to {destination_dir / folder_title}")
 
